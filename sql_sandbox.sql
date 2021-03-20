b6bac2372f950381f10b3e337ce1425a33480cae
Mert Gül <gul.mert89@gmail.com>
2021-03-20 14:44:58 +0300

    Fix the comment syntax

--- sql_sandbox.sql
+++ sql_sandbox.sql
@@ -1,4 +1,4 @@
-# Example-1
+--# Example-1
 SELECT 
 	customerNumber,
     customerName, 
@@ -7,14 +7,14 @@ SELECT
     ROUND(creditLimit, 0) AS creditLimit, 
     ROUND((creditLimit / 1.21), -2) AS creditLimit_Euro,
     creditLimit > 100000 AS isPremiumCustomer
-    # DISTINCT xyz    # and there is this DISTINCT thing.
+    -- DISTINCT xyz    -- and there is this DISTINCT thing.
 FROM classicmodels.customers
 WHERE country NOT IN ('USA', 'Australia') AND creditLimit > 0
-# WHERE country != 'USA'
+-- WHERE country != 'USA'
 ORDER BY customerNumber ASC
 LIMIT 20;
 
-# Example-2
+--# Example-2
 SELECT
 	rating,
 	# these are called "aggregate functions":
@@ -24,20 +24,20 @@ SELECT
     SUM(replacement_cost)
 FROM sakila.film
 WHERE rental_rate > 1.99
-GROUP BY 1    # 1st column. Could write "rating". GROUP BY is placed between these two.
+GROUP BY 1    -- 1st column. Could write "rating". GROUP BY is placed between these two.
 ORDER BY SUM(replacement_cost) DESC;
 
-# Example-3
+--# Example-3
 SELECT
 	salesRepEmployeeNumber,
-	COUNT(*),    # group by sütun ile gruplanan kayıtların satır sayısını verir.
+	COUNT(*),    -- group by sütun ile gruplanan kayıtların satır sayısını verir.
     ROUND(AVG(creditLimit), 0) AS creditLimit_avg, 
     ROUND(MAX(creditLimit), 0) AS creditLimit_max,
     ROUND(MIN(creditLimit), 0) AS creditLimit_min
 FROM classicmodels.customers
 GROUP BY salesRepEmployeeNumber;
 
-# Example-4
+-- Example-4
 SELECT
 	productLine,
     productScale,
@@ -48,17 +48,17 @@ GROUP BY
 	1, 2
 ORDER BY record_count DESC;
     
-# Example-5
+--# Example-5
 SELECT 
 	productLine,
     ROUND(AVG(buyPrice), 1) AS buyPrice_avg
 FROM classicmodels.products
 GROUP BY productLine
 HAVING buyPrice_avg >= 47
-# WHERE buyPrice_avg >= 48    # 'where' cannot be used w/ aggregate funcs.
+-- WHERE buyPrice_avg >= 48    -- 'where' cannot be used w/ aggregate funcs.
 ORDER BY buyPrice_avg ASC;
 
-# Example-6
+--# Example-6
 SELECT
 	ord.orderNumber, 
     ord.orderDate, 
@@ -66,7 +66,7 @@ SELECT
     cus.contactLastName,
     cus.contactFirstName
 FROM
-	orders ord    # "aliases' are used
+	orders ord    -- "aliases' are used
 JOIN
 	customers cus
 ON
@@ -75,7 +75,7 @@ LIMIT
 	20
 ;
 
-# Example-7
+-- Example-7
 SELECT
 	customerName, 
     contactLastName,
@@ -91,7 +91,7 @@ END "Representer"
 FROM classicmodels.customers
 ;
 
-# Example-8
+-- Example-8
 CREATE TABLE orderdetails_abbreviated AS
 SELECT
 	orderNumber,
@@ -102,7 +102,7 @@ LIMIT
 	20
 ;
 
-# Example-9
+--# Example-9
 -- No such table as "STUDENT
 -- Example output: "Student 3 has grade: A"
 SELECT
